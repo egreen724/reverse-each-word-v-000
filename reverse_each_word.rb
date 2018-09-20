d3143a3c4546c191a33f9e2fa39d4707a49208a0
Erin Green <erin.y.green@gmail.com>
2018-09-20 02:29:56 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
-def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
+#def reverse_each_word(sentence)
+ #sentence_array = sentence.split(" ")
   
-  sentence_array.collect do |word|
-    word.reverse!
-  end 
-  sentence_array.join(" ")
-end 
+  #sentence_array.collect do |word|
+   # word.reverse!
+  #end 
+  #sentence_array.join(" ")
+#end 
 
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
